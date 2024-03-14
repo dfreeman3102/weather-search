53d3aeec325bcf0a9aa991408e589ed6e22c7ac1
dfreeman3102 <davidfreeman3102@gmail.com>
2024-03-14 17:51:19 -0400

    added functionality to get from coords to city with actual weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 var button = document.querySelector(".search")
 var input = document.querySelector(".input")
 var apiKey = "409871a44bb3e3aa5098d1ea472cba80"
+var weatherdetails = (cityName, lat, lon) => {
+    var cityUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+    fetch(cityUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
 
 function getCoords() {
+    //entered city name, with removed spaces
     var cityName = input.value.trim();
-    var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
-    
-    fetch(cityUrl)
+    var coordUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+    //gets the coords and city name from the response
+    fetch(coordUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+           var {name, lat, lon} = data[0];
+            weatherdetails(name, lat, lon);
         })
 
 }
