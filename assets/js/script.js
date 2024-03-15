316567f0a4e7fc6fb46dd4afccc7346ca55b5d52
dfreeman3102 <davidfreeman3102@gmail.com>
2024-03-15 17:12:58 -0400

    added section to make dynamically updated html to reflect weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,37 @@
 var button = document.querySelector(".search")
 var input = document.querySelector(".input")
+var fivedaycards = document.querySelector("#five-day-forecast")
 var apiKey = "409871a44bb3e3aa5098d1ea472cba80"
+
+var weatherCard = (weatherData) => {
+    return `<section id="card">
+    <h5>Date:${weatherData.dt_txt.split(" ")[0]}</h4>
+    <p>Temp:${weatherData.main.temp}</p>
+    <p>${weatherData.wind.speed} MPH</p>
+    <p>${weatherData.main.humidity} %</p>
+</section>`;
+}
 var weatherdetails = (cityName, lat, lon) => {
-    var cityUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    var cityUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
     fetch(cityUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        //filters for only one forecast per day due to the new forecast every 3 hours
-        var days = [];
-        var fivedaycast = data.list.filter(forecast => {
-            var date = new Date(forecast.dt_txt).getDate();
-            if(!days.includes(date)){
-                return days.push(date);
-            }
+        .then(function (response) {
+            return response.json();
         })
-        console.log(fivedaycast);
-
-    })
+        .then(function (data) {
+            //filters for only one forecast per day due to the new forecast every 3 hours
+            var days = [];
+            var fivedaycast = data.list.filter(forecast => {
+                var date = new Date(forecast.dt_txt).getDate();
+                if (!days.includes(date)) {
+                    return days.push(date);
+                }
+            })
+            console.log(fivedaycast);
+            fivedaycast.forEach(weatherData => {
+                fivedaycards.insertAdjacentHTML("beforeend", weatherCard(weatherData));
+            })
+        })
 }
 
 function getCoords() {
@@ -33,7 +45,7 @@ function getCoords() {
         })
         .then(function (data) {
             console.log(data);
-           var {name, lat, lon} = data[0];
+            var { name, lat, lon } = data[0];
             weatherdetails(name, lat, lon);
         })
 
